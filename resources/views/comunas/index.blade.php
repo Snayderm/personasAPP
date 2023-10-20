54852ad6ccf97b3457a0a07c8d824343cd6c62d9
Snayderm <sneyder1002m@gmail.com>
2023-10-20 00:48:26 -0500

    index corregido

--- resources/views/comunas/index.blade.php
+++ resources/views/comunas/index.blade.php
@@ -31,7 +31,10 @@
                     <td>{{ $comuna->comu_nomb }}</td>
                     <td>{{ $comuna->muni_nomb }}</td>
                     <td>
-                      <h1>error</h1>
+                      <a href="{{route('comunas.edit',['comuna'=>$comuna->comu_codi]) }}"
+                        class="btn btn-info"> Edit </a></li>
+
+                      
                       <form action="{{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}"
                         method='POST' style="display: inline-block">
                         @method('delete')
@@ -40,7 +43,7 @@
                       </form>
                     </td>
                 </tr>
-                @endforeach
+                @endforeach          
             </tbody>
         </table>
     <div>
