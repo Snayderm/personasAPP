519259e6ac60b1ea718faf67f4d99c12e780d9c6
Snayderm <sneyder1002m@gmail.com>
2023-10-22 19:17:34 -0500

    Correcion vista dp y mp

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\MunicipioController;
 use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
 
